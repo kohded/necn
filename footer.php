1ff8c4dddb711250c0bcf04ebcc2943c27726de4
Lydia King <muckaluck525@yahoo.com>
2014-10-23 12:09:53 -0700

    Header Footer Nav
    First draft of the Header Footer Nav

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<p><small>Copyright &#169; 2014, Neighborhood Emergency Communications Network | All Rights Reserved</small></p>
+</footer>
+</div><!--Main closing div-->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+register_nav_menu("main_menu", "Main Menu");
+
+$mainMenu =  array (
+	"theme_location" => "main_menu",
+	"container" => "nav",
+	"container_class" => "",
+	"container_id" => "main_menu"
+);
+
+?>
