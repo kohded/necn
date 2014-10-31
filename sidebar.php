a6ee15134f08b313a9c3aeccb317d40a475a1a98
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-10-30 21:44:22 -0700

    update sidebar

--- sidebar.php
+++ sidebar.php
@@ -7,38 +7,39 @@
  * @version 1.0
  */
 ?>
-<!--begin sidebar - Need separate sidebar.pgp file for each Hub or add here?-->
+<!--begin sidebar -->
 <aside class="sidebar">
 	
-	<?php
-		$description = get_bloginfo( 'description', 'display' );
-		if ( ! empty ( $description ) ) :
-	?>
-	
-	<!-- Post ID and metadata; H2 tag, date, time, author -->
-	<article class="page" id="post-<?php the_ID(); ?>">
-	
-	<h2 class="post-description"><?php echo esc_html( $description ); ?></h2>
-		<p class="postmetadata">
-			<span class="">Posted on
-				<?php the_time('F jS, Y') ?>
-			</span>
-			<span class="">
-				<?php _e('By');?>
-				<?php the_author_posts_link(); ?>
-			</span>
-		</p>
-	</article>
-	
-	<?php
-		endif;
-	?>
+<?php // if is not either the 'not found' or 'search' pages
+	if ( !is_404() ) { if ( !is_search() ) {
+?>
+	<!-- begin Sub Pages-->
+<?php
+	if ($post->post_parent) { // if has a post parent
+	$children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0" );
+	$parentname = get_the_title($post->post_parent);
+		} else { // if does not have a post parent
+	$children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0" );
+$parentname = get_the_title($post->ID);
+}
+	if ($children) { // if has children ?>
+
+		<div id="sub-navigation" class="widget-area">
+	<h2 id="sub-navigation-title"><?php echo $parentname; ?> </h2>
+			<ul id="sub-navigation-items">
+				<?php echo $children; ?>
+			</ul>
+		</div>
+<?php }
+
+?>
+<!-- End Sub Pages -->
 
 	<?php
 		if ( is_active_sidebar( 'content' ) ) :
 	?>
 	
-	<div id="content-sidebar" class="content-sidebar widget-area" role="complementary">
+	<div class="content-sidebar widget-area" role="complementary">
 		<?php dynamic_sidebar( 'content' ); ?>
 	</div>
 	<!-- end content-sidebar -->
@@ -50,7 +51,7 @@
 	<?php
 		if ( is_active_sidebar( 'primary' ) ) :
 	?>
-	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+	<div class="primary-sidebar widget-area" role="complementary">
 		<?php dynamic_sidebar( 'primary' ); ?>
 	</div>
 	<!-- end primary-sidebar -->
