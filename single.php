8d213baf3a139c290e8a7ea0723f47e029db682f
Lydia King <muckaluck525@yahoo.com>
2014-11-03 22:13:41 -0800

    04 Nov Update

--- single.php
+++ single.php
@@ -10,12 +10,13 @@
 	<?php if (have_posts()) :  ?>
 	<?php while(have_posts()) : the_post(); ?>
 	<h2><?php the_title(); ?></h2>
-	<p><small>Posted
-		<time datetime="<?php the_time('Y-m-d'); ?>"></time>
-		by <?php the_author(); ?>,
-		</small></p>
+	<p><small>Posted by <?php the_author(); ?> </small></p>
 		<?php the_content(); ?>
 		<p>Listed in: <?php the_category(", ") ?></p>
+		<ul class="post-nav">
+			<?php previous_post_link("<li>%link</li>", "&lt; Previous"); ?>
+			<?php next_post_link("<li>%link</li>", "Next &gt;"); ?>
+		</ul>
 		<?php comments_template(); ?>
 	<?php endwhile; ?>
 	<?php endif; ?>
