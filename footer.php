7e3c6cae8d0a8b7922ccc6da357e01213d7e25d5
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-11-18 19:50:56 -0800

    footer.php add class content
    Add content div to footer

--- footer.php
+++ footer.php
@@ -1,11 +1,15 @@
 </div><!--content closing div-->
 <footer>
+	<!--start footer class content div-->
+	<div class="content">
      <div class="footer-nav">
 					<?php wp_nav_menu($footerMenu);?>
 				</div>
 
         <?php get_sidebar("footer"); ?>
 	<p><small>Copyright &#169; 2014, Neighborhood Emergency Communications Network | All Rights Reserved</small></p>
+	</div>
+	<!--end footer class content div-->
 </footer>
 <?php wp_footer(); ?>
 </body>
