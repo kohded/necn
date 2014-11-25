d1905a5615e0a9cc31e69e3fd2b78090f4152a8e
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-11-25 14:22:02 -0800

    neighborhood.php
    show content of post on neighborhood page

--- neighborhood.php
+++ neighborhood.php
@@ -29,9 +29,8 @@ Template Name: Neighborhood
         <?php while ( have_posts() ) : the_post(); ?>
             <article class="article-post">
 		<h2 id="post-<?php the_ID(); ?>" class="post-title">
-			<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a>
 		</h2>
-                <?php the_excerpt('More &raquo;'); ?><a href="<?php the_permalink() ?>">Read More &raquo;</a>
+                <?php the_content() ?>
             </article>
         <?php endwhile; ?>
     <?php endif; ?>
