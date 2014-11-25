12419a81f9d733314cdd8dc21be4dd8cfe7ba207
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-11-25 15:06:28 -0800

    header.php add GAC
    Added Google Ac in the header

--- header.php
+++ header.php
@@ -7,7 +7,18 @@
 		<title><?php bloginfo("name") . wp_title('|'); ?></title>
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<!-- start Google AC -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-36348141-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- end Google AC -->
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class($class) ?>>
