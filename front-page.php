03c8fa3cd5518beb6d4d1957a24d8470654978a5
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-11-25 17:42:51 -0800

    front-page.php
    change to content instead of excerpt

--- front-page.php
+++ front-page.php
@@ -14,19 +14,6 @@
     		<!-- end front-page slider -->        
 <!-- start content -->
 	<div class="content-area">		
-  	
-  	<h1>Neighborhood Emergency Communications Network</h1>
-	&nbsp;
-	<h2>NECN - Prepare!</h2>
-             <p>Are you prepared for natural disasters, or other threats?  If a major earthquake hits Seattle, where will you and your family get drinking water and food?
-	     How will you communicate with your spouse or your family members if all the local phone lines are down, including cell towers?  Do you have a emergency preparedness plan?</p>
-	     &nbsp;
-	     <p>City officials may not be able to help you in the case of a major natural disaster, terrorism attack or city, county or state wide emergency, so
-	     the city is urging neighborhoods, families and individuals to be prepared to help themselves and each other.</p>
-	     &nbsp;
-	     <p>This website is created to help prepare you, your family, and your neighborhood for a disaster.  Find out what your
-	     neighborhood is doing to be prepared, and how you can help!</p>
-             &nbsp;
   
         <!-- start posts -->            
             	
@@ -37,7 +24,7 @@
 			<h2 id="post-<?php the_ID(); ?>" class="post-title">
                     <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a>
              </h2>
-             <?php the_excerpt('More &raquo;'); ?><a href="<?php the_permalink() ?>">Read More &raquo;</a> 
+             <?php the_content('More &raquo;'); ?><a href="<?php the_permalink() ?>">Read More &raquo;</a> 
 
    		 </article>
 	<?php endwhile; ?>
