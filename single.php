57b45bc6fced8612bd4bca1d1e26016fc728ce69
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-11-27 15:58:47 -0800

    single.php
    added single quotes within function on line 15
    the_content(' ')

--- single.php
+++ single.php
@@ -6,12 +6,13 @@
 
 <?php get_header(); ?>
 
+        <!-- start content -->
 <div class="main-content-left">
 	<?php if (have_posts()) :  ?>
 	<?php while(have_posts()) : the_post(); ?>
 	<h2><?php the_title(); ?></h2>
-	<p><small>Posted by <?php the_author(); ?> </small></p>
-		<?php the_content(); ?>
+	<p><small>Posted by: <?php the_author(); ?></small></p>
+		<?php the_content(''); ?>
 		<p>Listed in: <?php the_category(", ") ?></p>
 		<ul class="post-nav">
 			<?php previous_post_link("<li>%link</li>", "&lt; Previous"); ?>
