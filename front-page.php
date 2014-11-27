e662b6d06d9dad197161e37825c3cc7f36204168
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-11-27 15:50:39 -0800

    front-page.php show content
    add SEO title tag, show content, then show latest 3 posts

--- front-page.php
+++ front-page.php
@@ -4,7 +4,6 @@
 */
 ?>
 <?php get_header(); ?>
-
 		
     		<!-- start front-page slider -->
     <div id="slider-home">
@@ -12,27 +11,33 @@
     echo do_shortcode("[metaslider id=334]"); 
 ?>    </div>
     		<!-- end front-page slider -->        
+
 <!-- start content -->
-	<div class="content-area">		
-  
+	<div class="content-area">
         <!-- start posts -->            
             	
-	<?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?> 	   
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php the_content(''); ?>
+	<?php endwhile; endif; ?>
+	<!-- end this loop -->
+	
+	<!-- start second loop show latest 3 posts-->
+	<?php rewind_posts(); //stop the first loop ?>
+	<?php query_posts('showposts=3'); //show latest 3 posts ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<article class="article-post">
 
-			<h2 id="post-<?php the_ID(); ?>" class="post-title">
+             <h2 id="post-<?php the_ID(); ?>" class="post-title">
                     <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a>
              </h2>
 
              <?php the_excerpt(); ?><a href="<?php the_permalink() ?>">Read More &raquo;</a> 
-            
-
+             <p><small>Posted by <?php the_author(); ?> in <?php the_category(', ') ?></small></p>
 
    		 </article>
 	<?php endwhile; ?>
         <?php endif; ?>
-        <!-- end posts -->
+        <!-- end second loop of posts -->
         <?php wp_link_pages(); ?>
              
     </div>
