aa600eefd1cd069e21076812b43ca0d992094c13
ForrestMcIntyre <forrest.mcintyre@gmail.com>
2014-11-28 00:45:47 -0800

    header.php
    added Typekit code for Futura

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
+    	<script src="//use.typekit.net/scx6gfo.js"></script>
+		<script>try{Typekit.load();}catch(e){}</script>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<!-- snippet for wp responsive design -->
 		
