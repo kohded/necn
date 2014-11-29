812b3102d13e2348e8f339c2b62cb622027bab4f
Patricia Beth Barker <patriciabethbarker@gmail.com>
2014-11-28 16:21:34 -0800

    header.php
    add the meta description tag

--- header.php
+++ header.php
@@ -3,12 +3,18 @@
 	<head>
     	<script src="//use.typekit.net/scx6gfo.js"></script>
 		<script>try{Typekit.load();}catch(e){}</script>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<!-- snippet for wp responsive design -->
 		
+		<!-- snippet for wp responsive design -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 		<!-- SEO get_my_title_tag -->
 		<title><?php get_my_title_tag(); ?></title>
+		
+		<!-- Start Meta -->
 		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="description" content="<?php echo strip_tags(get_the_excerpt()); ?>" />
+		
+		<!-- Start Stylesheet -->
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 <!-- start Google AC -->
 <script>
